e3da853f40ad4779cf0f5adf5cf39371b04fd968
maguro <maguro.devlife@gmail.com>
2022-05-12 09:06:50 +0900

    added navigation link

--- packages/web/components/Header.tsx
+++ packages/web/components/Header.tsx
@@ -1,9 +1,35 @@
 import Image from "next/image";
+import Link from "next/link";
 import logo from "../assets/logo.svg";
 const Header = () => {
+  const menus = [
+    {
+      path: "/mint",
+      name: "Mint",
+    },
+    {
+      path: "/stake",
+      name: "Stake",
+    },
+    {
+      path: "/profile",
+      name: "Profile",
+    },
+  ];
   return (
-    <header className="bg-cyan-500 p-5">
-      <Image src={logo} alt="logo" />
+    <header className="flex justify-between bg-cyan-500 p-5">
+      <Link href="/">
+        <a>
+          <Image src={logo} alt="logo" />
+        </a>
+      </Link>
+      <div className="flex gap-7 text-white font-bold items-center">
+        {menus.map((menu, i) => (
+          <Link href={menu.path} key={i}>
+            <a>{menu.name}</a>
+          </Link>
+        ))}
+      </div>
     </header>
   );
 };
